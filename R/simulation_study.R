010262bdb16a993ebc2ea4714273bab3f51c535b
Dongyue Xie <dongyxie@gmail.com>
2022-10-28 14:09:40 -0500

    revise simulation func

--- R/simulation_study.R
+++ R/simulation_study.R
@@ -1,15 +1,20 @@
 
-#'@title generate simulation data
+#'@title generate simulation data, gamma
+#'@details The DGP is
+#'\deqn{x_i\sim Poisson(\lambda_i),}
+#'\deqn{\lambda_i\sim w\times Gamma(shape0,rate0) + (1-w)\times Gamma(shape1,rate1).}
 #'@export
 
-gen_data_identity = function(n=1e3,n_simu=100,
+gen_data_gamma = function(n=1e3,n_simu=100,
                           prior='gamma',
-                          gamma_shape = 10,
-                          gamma_rate=2,
-                          t_df = 3,
-                          t_delta=2,
-                          quasi_sparse=0.1,
-                          exp_rate = 0.5,
+                          shape0 = 2,
+                          rate0=2,
+                          shape1 = 10,
+                          rate1=2,
+                          #t_df = 3,
+                          #t_delta=2,
+                          #quasi_sparse=0.1,
+                          #exp_rate = 0.5,
                           w = 0.8,
                           seed=12345){
 
@@ -19,16 +24,36 @@ gen_data_identity = function(n=1e3,n_simu=100,
   X = matrix(nrow=n_simu,ncol=n)
   if(prior=='gamma'){
     for(i in 1:n_simu){
-      d1 = rgamma(n,gamma_shape,gamma_rate)
+      d0 = rgamma(n,shape0,rate0)
+      d1 = rgamma(n,shape1,rate1)
       idx = rbinom(n,1,1-w)
-      l = idx*d1 + (1-idx)*quasi_sparse
+      l = idx*d1 + (1-idx)*d0
       Mean[i,] = l
       X[i,] = rpois(n,l)
     }
-    return(list(Mean=Mean,X=X,log_Mean = log(Mean),prior=prior,gamma_shape=gamma_shape,
-                gamma_rate=gamma_rate,quasi_sparse=quasi_sparse,w=w,seed=seed))
+    return(list(Mean=Mean,X=X,log_Mean = log(Mean),prior=prior,shape0=shape0,
+                rate0=rate0,shape1=shape1,rate1=rate1,w=w,seed=seed))
   }
+}
 
+#'@title generate simulation data, t
+#'@details The DGP is
+#'\deqn{x_i\sim Poisson(\lambda_i),}
+#'\deqn{\lambda_i\sim w\times \delta_{c} + (1-w)\times (|t_{df}|+delta_{2c}).}
+#'@export
+
+gen_data_t = function(n=1e3,n_simu=100,
+                          prior='t',
+                          t_df = 3,
+                          t_delta=2,
+                          #exp_rate = 0.5,
+                          w = 0.8,
+                          seed=12345){
+
+  set.seed(seed)
+
+  Mean = matrix(nrow=n_simu,ncol=n)
+  X = matrix(nrow=n_simu,ncol=n)
   if(prior=='t'){
     for(i in 1:n_simu){
       d1 = abs(rt(n,t_df))+t_delta*2
@@ -39,6 +64,23 @@ gen_data_identity = function(n=1e3,n_simu=100,
     }
     return(list(Mean=Mean,X=X,log_Mean = log(Mean),prior=prior,t_df=t_df,t_delta=t_delta,w=w,seed=seed))
   }
+}
+
+#'@title generate simulation data, exponential
+#'@details The DGP is
+#'\deqn{x_i\sim Poisson(\lambda_i),}
+#'\deqn{\lambda_i\sim Exp(rate).}
+#'@export
+
+gen_data_exp = function(n=1e3,n_simu=100,
+                      prior='exponential',
+                      exp_rate = 0.5,
+                      seed=12345){
+
+  set.seed(seed)
+
+  Mean = matrix(nrow=n_simu,ncol=n)
+  X = matrix(nrow=n_simu,ncol=n)
 
   if(prior=='exponential'){
     for(i in 1:n_simu){
@@ -48,12 +90,19 @@ gen_data_identity = function(n=1e3,n_simu=100,
     }
     return(list(Mean=Mean,X=X,log_Mean = log(Mean),prior=prior,exp_rate=exp_rate,seed=seed))
   }
-
 }
 
-#'@title generate simulation data
+
+
+
+#'@title generate simulation data, log-link
+#'@details The DGP is
+#'\deqn{x_i\sim Poisson(\exp(\mu_i)),}
+#'\deqn{\mu_i\sim w\times N(m,\sigma^2_0) + (1-w)\times N(m,\sigma^2_1).}
 #'@export
-gen_data_exp = function(n=1e3,n_simu=100,w=0.8,prior_mean=2,prior_var0 = 0, prior_var1 = 3,seed=12345){
+gen_data_log_link = function(n=1e3,n_simu=100,w=0.8,
+                             prior_mean=2,prior_var0 = 0,
+                             prior_var1 = 2,seed=12345){
   set.seed(seed)
 
   Mu = matrix(nrow=n_simu,ncol=n)
