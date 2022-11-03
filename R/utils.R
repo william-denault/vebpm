dda47a8d1f11e6354ff93ca403cd66ec3d880ac9
Dongyue Xie <dongyxie@gmail.com>
2022-11-03 15:17:02 -0500

    export utils

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,11 @@
 #'@title MSE
+#'@export
 mse = function(x,y){mean((x-y)^2)}
 
 #'@title RMSE
+#'@export
 rmse = function(x,y){sqrt(mean((x-y)^2))}
 
 #'@title MAE
+#'@export
 mae = function(x,y){mean(abs(x-y))}
